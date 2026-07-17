5b6c458a4eed4d8de4d24a40e8bdd1045870bf70
Kalyank1904 <kalyank1904@gmail.com>
2026-07-17 15:09:37 +0100

    Fix config (load_libraries, libraries list) and cache raw row/column counts before cleaning

--- munge/01-A-merge-data.R
+++ munge/01-A-merge-data.R
@@ -1,13 +1,17 @@
-# Merge data
-
 year_object_names <- ls(envir = .GlobalEnv, pattern = "^movies_20[0-9]{2}$")
 
 year_dfs <- mget(year_object_names, envir = .GlobalEnv)
 
 movies_raw <- dplyr::bind_rows(year_dfs, .id = "source_file_id")
 
-cat("Number of rows after merging:", nrow(movies_raw), "\n")
-cat("Number of columns:", ncol(movies_raw), "\n")
+raw_row_count <- nrow(movies_raw)
+raw_col_count <- ncol(movies_raw)
+
+cat("Number of rows after merging:", raw_row_count, "\n")
+cat("Number of columns:", raw_col_count, "\n")
 
 rm(list = year_object_names, envir = .GlobalEnv)
 rm(year_dfs, year_object_names)
+
+ProjectTemplate::cache("raw_row_count")
+ProjectTemplate::cache("raw_col_count")
