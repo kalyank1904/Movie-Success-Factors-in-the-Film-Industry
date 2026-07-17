7eb358f07eee2c94664997047ebba0505888f6df
Kalyank1904 <kalyank1904@gmail.com>
2026-07-17 13:38:40 +0100

    Add data cleaning script: clean_names, type coercion, deduplication

---
+++ munge/02-B-clean-data.R
@@ -0,0 +1,33 @@
+movies <- movies_raw %>%
+  janitor::clean_names()
+
+glimpse(movies)
+
+movies <- movies %>%
+  mutate(
+    id                    = as.character(id),
+    title                 = as.character(title),
+    vote_average          = as.numeric(vote_average),
+    vote_count            = as.numeric(vote_count),
+    status                = as.character(status),
+    release_date          = as.Date(release_date),
+    revenue               = as.numeric(revenue),
+    runtime               = as.numeric(runtime),
+    adult                 = as.logical(adult),
+    budget                = as.numeric(budget),
+    original_language     = as.character(original_language),
+    original_title        = as.character(original_title),
+    overview              = as.character(overview),
+    popularity            = as.numeric(popularity),
+    tagline               = as.character(tagline),
+    genres                = as.character(genres),
+    production_companies  = as.character(production_companies),
+    production_countries  = as.character(production_countries),
+    spoken_languages      = as.character(spoken_languages),
+    keywords              = as.character(keywords)
+  )
+
+movies <- movies %>%
+  distinct(id, .keep_all = TRUE)
+
+cat("Rows after removing duplicates:", nrow(movies), "\n")
